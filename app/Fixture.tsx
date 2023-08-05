1cf213f7a80ce794de4bca5b3bb538b480418938
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-08-05 22:43:48 +0200

    a in a

--- app/Fixture.tsx
+++ app/Fixture.tsx
@@ -9,27 +9,29 @@ interface IFixtureProps {
 
 const Fixture: React.FC<IFixtureProps> = (props: IFixtureProps) => {
   return (
-    <Link
-      className="grid gap-1 bg-surface rounded-lg text-onSurface p-2"
-      href={"/fixture/" + props.data.id}
-    >
-      <h3 className="font-bold capitalize grid grid-flow-col">
-        {props.data.manufacture.name} - {props.data.model}
-        {props.edit && <ManageIcons fixture={props.data.id}/>}
-      </h3>
-      <div className="grid grid-flow-col">
-        <div>
-          <span className="font-bold">Waga: </span>
-          <span className="capitalize">{props.data.weight} Kg</span>
-        </div>
-        {props.data.power && (
+    <div className="grid grid-flow-col bg-surface rounded-lg p-2">
+      <Link
+        className="grid gap-1 text-onSurface"
+        href={"/fixture/" + props.data.id}
+      >
+        <h3 className="font-bold capitalize grid grid-flow-col">
+          {props.data.manufacture.name} - {props.data.model}
+        </h3>
+        <div className="grid grid-flow-col">
           <div>
-            <span className="font-bold">Moc: </span>
-            <span className="capitalize">{props.data.power} W</span>
+            <span className="font-bold">Waga: </span>
+            <span className="capitalize">{props.data.weight} Kg</span>
           </div>
-        )}
-      </div>
-    </Link>
+          {props.data.power && (
+            <div>
+              <span className="font-bold">Moc: </span>
+              <span className="capitalize">{props.data.power} W</span>
+            </div>
+          )}
+        </div>
+      </Link>
+      {props.edit && <ManageIcons fixture={props.data.id} />}
+    </div>
   );
 };
 
